e974430bf9a9d79f816c8ef641054124890ea7e2
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-02-19 17:23:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ bot.on('guildMemberAdd', member => {
     }).catch(console.error)
 });
 
-const ban = require('./kick et ban/ban.js');
-require('./embed/RichEmbed');
+const ban = require('ban.js');
+require('RichEmbed.js');
 
 bot.on('message', function (message){
     if (ban.match(message)){
