d4333e5356b68f435c3028108aa56ab7c8b8d632
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-03-24 14:22:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+/*
+const cfg = require('./index.json'); // a garder en version desktop
+*/
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("/");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+    bot.user.setActivity('Surveiller').catch(console.error)
 });
-
+bot.login(token)
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
-        console.log(`${member.displayName} à rejoind le serveur.`)
+        return channel.send('Bienvenue sur le serveur, va lire les règles et appui sur la réaction pour avoir ton grade' + member.displayName)
+        bot.on(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
 
-const ban = require('./kick et ban/ban');
-require('./embed/RichEmbed');
-
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});
 
 
 bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
+    if (msg.content === "kick"){
+        msg.reply("Ne reviens plus.")
     }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
+    if (msg.content.match(/mpx en force/i)) {
+            msg.reply('Je suis d\'accord avec toi la mpx est la meileure.')
     }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
+    if (msg.content === prefix + "chaine"){
+        msg.channel.send("https://www.youtube.com/channel/UCOhGDL7a89EkC91H6Kp-Alg?view_as=subscriber")
+        console.log("Une personne a demandé pour aller sur ta chaine.")
     }
-
+ 
 });
 
 bot.login(token); //a garder en version heroku
