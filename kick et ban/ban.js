108403ca997b199fd15e4b84c7fccc27b7fc7c3a
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-12 18:10:18 +0200

    Update ban.js

--- kick et ban/ban.js
+++ kick et ban/ban.js
@@ -18,24 +18,3 @@ module.exports = class ban {
       }
     //}
 }
-
-const prefix = ("-");
-
-module.exports = class kick {
-    static match (message) {
-      return message.content.startsWith('-kick')
-    }
-            
-    static action (message) {
-      const args = message.content.slice(prefix.length).split(/ +/);
-      const command = args.shift().toLowerCase();
-        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply('tu n\'est pas admin . ');
-          const member = message.mentions.members.first();
-        if (!member) return message.reply('Commande Invalide, il faut mettre `-kick @User#1234`');
-          member.kick({
-            days: args[1] || null,
-            reason: `Expluser par ${message.author.tag}`
-          });
-      }
-    //}
-}
