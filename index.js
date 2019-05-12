70ddd3508e43988dec5cc03fdb8756122d100abc
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-12 19:04:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ bot.on('message', function (message){
         }).catch(() => {
              // Failmessage
             message.channel.send("Impossible d/executer cette commande !");
+            
     }
 });
 
