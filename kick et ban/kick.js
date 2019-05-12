f032a517c5d87486baa3989a63852c9dea8a7234
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-12 19:00:05 +0200

    Update kick.js

--- kick et ban/kick.js
+++ kick et ban/kick.js
@@ -1,4 +1,4 @@
-user.on("message", (message) => {
+client.on("message", (message) => {
     if (message.content.startsWith("-kick")) {
         // Easy way to get member object though mentions.
         var member= message.mentions.members.first();
