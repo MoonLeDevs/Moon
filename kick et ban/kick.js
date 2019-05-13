5176c8772fd26a45393b68e7cc3e2c09a545d7e4
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-13 13:30:57 +0200

    Update kick.js

--- kick et ban/kick.js
+++ kick et ban/kick.js
@@ -1,24 +1,24 @@
 {
-          if (!m.guild) return
-          if(!m.member.hasPermission("KICK_MEMBERS"));
-          if (m.content.startsWith(".kick")) {
-    let auth = m.author
-    let user = m.mentions.users.first();
+          if (!message.guild) return
+          if(!message.member.hasPermission("KICK_MEMBERS"));
+          if (message.content.startsWith(".kick")) {
+    let auth = message.author
+    let user = message.mentions.users.first();
           if (user) {
-    const member = m.guild.member(user);
+    const member = message.guild.member(user);
           if (member) {
                 member.kick("**Bye bye").then(() => {
-                m.reply(`${user.tag} a été kick`);
+                message.reply(`${user.tag} a été kick`);
                        }).catch(err => {
-                m.reply('I was unable to kick the member');
+                message.reply('I was unable to kick the member');
                        console.error(err);
                   });
            } else {
-                m.reply('That user isn\'t in this guild!');
+                message.reply('That user isn\'t in this guild!');
            }
     } else {
-                m.reply('You didn\'t mention the user to kick!');
-                m.delete()
+                message.reply('You didn\'t mention the user to kick!');
+                message.delete()
               }
        }
 }
