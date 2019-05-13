2eb0f0ec5968cf3b5d4771373f7dfef7c5986e54
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-13 12:10:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,4 @@ bot.on('message', function (message){
         return ban.action(message)
     }
 });
-    }     
- bot.on(message.content.startsWith("-kick")) {
-    if member= message.mentions.members.first();
-        member.kick().then((member) => {
-            message.channel.send(":wave: " + member.displayName + " a été expulser ");
-        }).catch(() => {
-            message.channel.send("Impossible d/executer cette commande !");        
-    }
-});
-
 bot.login(token);
