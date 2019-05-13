c10b205bc0364f4844fd82140c165203357571b9
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-13 12:10:58 +0200

    Update kick.js

--- kick et ban/kick.js
+++ kick et ban/kick.js
@@ -1,1 +1,8 @@
-
+ bot.on(message.content.startsWith("-kick")) {
+    if member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(":wave: " + member.displayName + " a été expulser ");
+        }).catch(() => {
+            message.channel.send("Impossible d/executer cette commande !");        
+    }
+});
