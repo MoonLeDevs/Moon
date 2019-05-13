85491490fa6e676a54483fe6b3b52acf4182f492
MoonLeDevs <47786430+MoonLeDevs@users.noreply.github.com>
2019-05-13 12:15:05 +0200

    Update kick.js

--- kick et ban/kick.js
+++ kick et ban/kick.js
@@ -1,8 +1,18 @@
- bot.on(message.content.startsWith("-kick")) {
-    if member= message.mentions.members.first();
-        member.kick().then((member) => {
-            message.channel.send(":wave: " + member.displayName + " a été expulser ");
-        }).catch(() => {
-            message.channel.send("Impossible d/executer cette commande !");        
+const prefix = ("-");
+
+module.exports = class kick {
+    static match (message) {
+      return message.content.startsWith('-kick')
     }
-});
+            
+    static action (message) {
+      const args = message.content.slice(prefix.length).split(/ +/);
+      const command = args.shift().toLowerCase();
+        if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply('Accès Refusé : Permission **Admin** Requise. ');
+          const member = message.mentions.members.first();
+        if (!member) return message.reply('Commande Invalide, il faut mettre `-kick @user#1234`');
+          member.kick({
+          });
+      }
+    //}
+}
